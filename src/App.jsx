e9cb09b32418605e7a5051ef114dd94ddd6d3bb7
kellylee23 <dmstj4632@hufs.ac.kr>
2025-08-14 01:44:29 +0900

    feat: bottomNav수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Suspense, lazy } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import GlobalStyles from './styles/globalstyles'
+import Layout from './components/nav/Layout'
 
 const Home = lazy(() => import('./pages/Home'))
 const Login = lazy(() => import('./pages/Auth/Login'))
@@ -21,6 +22,7 @@ const ProfileSell = lazy(() => import('./pages/ProfileSell'))
 const ProfileBuy = lazy(() => import('./pages/ProfileBuy'))
 
 function App() {
+  const userType = 'seller'
   return (
     <Router>
       <GlobalStyles />
@@ -30,47 +32,49 @@ function App() {
           <Route path="/auth/login" element={<Login />} />
           <Route path="/auth/signup" element={<Signup />} />
 
-          {/* ######### 2, 3 #########*/}
-          {/* 상품 전체 확인 */}
-          <Route path="/product" element={<ProductList />} />
-          {/* 상품 상세 */}
-          <Route path="/product/:id" element={<ProductDetail />} />
-          {/* 주문 */}
-          <Route path="/order" element={<Order />} />
-          {/* 장바구니 */}
-          <Route path="/product/cart" element={<ProductCart />} />
+          <Route element={<Layout userType={userType} />}>
+            {/* ######### 2, 3 #########*/}
+            {/* 상품 전체 확인 */}
+            <Route path="/product" element={<ProductList />} />
+            {/* 상품 상세 */}
+            <Route path="/product/:id" element={<ProductDetail />} />
+            {/* 주문 */}
+            <Route path="/order" element={<Order />} />
+            {/* 장바구니 */}
+            <Route path="/product/cart" element={<ProductCart />} />
 
-          {/* ######### 4 #########*/}
-          {/* 공동구매 */}
-          <Route path="/groupbuy" element={<GroupBuy />} />
-          {/* 공동구매 생성 */}
-          <Route path="/groupbuy/create" element={<GroupBuyCreate />} />
-          {/* 공동구매 신청 */}
-          <Route path="/groupbuy/regi" element={<GroupBuyRegi />} />
+            {/* ######### 4 #########*/}
+            {/* 공동구매 */}
+            <Route path="/groupbuy" element={<GroupBuy />} />
+            {/* 공동구매 생성 */}
+            <Route path="/groupbuy/create" element={<GroupBuyCreate />} />
+            {/* 공동구매 신청 */}
+            <Route path="/groupbuy/regi" element={<GroupBuyRegi />} />
 
-          {/* ######### 5 #########*/}
-          {/* 캘린더 */}
-          <Route path="/calendar" element={<Calendar />} />
+            {/* ######### 5 #########*/}
+            {/* 캘린더 */}
+            <Route path="/calendar" element={<Calendar />} />
 
-          {/* ######### 6 #########*/}
-          {/* 상품관리 */}
-          <Route path="/product/manage" element={<ProductManage />} />
-          {/* 프로필-판매자 */}
-          <Route path="/profile/sell" element={<ProfileSell />} />
-          {/* 프로필-구매자 */}
-          <Route path="/profile/buy" element={<ProfileBuy />} />
-          {/* 상품등록-텍스트 */}
-          <Route
-            path="/product/register/text"
-            element={<ProductRegisterText />}
-          />
-          {/* 상품등록-품질관리(신선도관리)를 위한 이미지 촬영 */}
-          <Route
-            path="/product/register/image"
-            element={<ProductRegisterImage />}
-          />
-          {/* 신선도 결과 */}
-          <Route path="/freshness" element={<FreshnessResult />} />
+            {/* ######### 6 #########*/}
+            {/* 상품관리 */}
+            <Route path="/product/manage" element={<ProductManage />} />
+            {/* 프로필-판매자 */}
+            <Route path="/profile/sell" element={<ProfileSell />} />
+            {/* 프로필-구매자 */}
+            <Route path="/profile/buy" element={<ProfileBuy />} />
+            {/* 상품등록-텍스트 */}
+            <Route
+              path="/product/register/text"
+              element={<ProductRegisterText />}
+            />
+            {/* 상품등록-품질관리(신선도관리)를 위한 이미지 촬영 */}
+            <Route
+              path="/product/register/image"
+              element={<ProductRegisterImage />}
+            />
+            {/* 신선도 결과 */}
+            <Route path="/freshness" element={<FreshnessResult />} />
+          </Route>
         </Routes>
       </Suspense>
     </Router>
---
+++ src/components/nav/Layout.jsx
@@ -0,0 +1,13 @@
+import { Outlet } from 'react-router-dom'
+import BottomNav from './BottomNav'
+
+export default function Layout({ userType }) {
+  return (
+    <div style={{ paddingBottom: '85px' }}>
+      {' '}
+      {/* Nav 높이만큼 padding */}
+      <Outlet /> {/* 여기에 각 페이지의 내용이 렌더링됨 */}
+      <BottomNav userType={userType} />
+    </div>
+  )
+}
