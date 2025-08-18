f9e8bd8d3ebdbd158e0f9c2fc32103f6a9606b42
이지우 <dlwldn041020@hufs.ac.kr>
2025-08-19 01:22:17 +0900

    구매자 프로필

---
+++ src/components/profile/UserInformationItem.jsx
@@ -0,0 +1,61 @@
+import styled from '@emotion/styled'
+
+const InfoContainer = styled.div`
+  width: 352px;
+  height: auto;
+  border-radius: 24px;
+  background: rgba(255, 255, 224, 0.50);
+  box-shadow: 0 4px 4px 0 rgba(0, 0, 0, 0.25);
+  padding: 26.72px 13.78px;
+  margin: 13px auto 0 auto;
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  gap: 18px;
+`
+
+const InfoField = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 19px;
+  font-size: 16px;
+`
+
+const Label = styled.p`
+  margin: 0;
+  min-width: 60px;
+  font-size: 16px;
+  white-space: nowrap;
+`
+
+const InfoValue = styled.span`
+  display: inline-block;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.5);
+  padding: 4px 0;
+  width: 260px;
+  line-height: 20px;
+`
+
+export default function UserInformationItem({
+  name = '홍길동',
+  id = '멋쟁이사자',
+  phoneNumber = '010-1234-5678',
+}) {
+  return (
+    <InfoContainer>
+      <InfoField>
+        <Label>이름</Label>
+        <InfoValue>{name}</InfoValue>
+      </InfoField>
+      <InfoField>
+        <Label>아이디</Label>
+        <InfoValue>{id}</InfoValue>
+      </InfoField>
+      <InfoField>
+        <Label>전화번호</Label>
+        <InfoValue>{phoneNumber}</InfoValue>
+      </InfoField>
+    </InfoContainer>
+  )
+}
--- src/pages/ProfileBuy.jsx
+++ src/pages/ProfileBuy.jsx
@@ -1,5 +1,82 @@
 import React from 'react'
+import MainHeader from '../components/nav/Header'
+import styled from '@emotion/styled'
+import UserInformationItem from '../components/profile/UserInformationItem'
+import StoreInfoContainer from '../components/profile/StoreInfoCard'
+
+const Container = styled.div`
+  position: relative;
+  width: 100%;
+  min-height: 100vh;
+  max-width: 393px; /* 모바일 최대 폭 */
+  margin: 0 auto;
+  background: #ffffff;
+  min-height: 100vh;
+  height: auto;
+  overflow-y: auto;
+  padding-bottom: 90px;
+`
+
+const MainContainer = styled.div`
+  padding: 17px;
+
+`
+const Title = styled.div`
+  color: #000;
+  font-family: Pretendard;
+  font-size: 24px;
+  letter-spacing: -0.96px;
+`
+
+const RegisterButton = styled.div`
+  display: flex;
+  width: 148px;
+  height: 31px;
+  box-sizing: border-box;
+  padding: 7px 44px 5px 43px;
+  justify-content: center;
+  align-items: center;
+  border-radius: 10px;
+  background: #1F3906;
+  color: #fff;
+  margin: 0 auto;
+  margin-top: 16px;
+`
 
 export default function ProfileBuy() {
-  return <div></div>
+  return (
+    <Container>
+      <MainHeader />
+      <MainContainer>
+        <Title>반갑습니다, 머쨍이님</Title>
+        <p
+            style={{
+              color: '#000',
+              WebkitTextStrokeWidth: '0.2px',
+              WebkitTextStrokeColor: '#000',
+              fontSize: '16px',
+              margin: '0',
+              marginTop: '30px',
+            }}
+          >
+            나의 정보
+          </p>
+         <UserInformationItem />
+         <p
+            style={{
+              color: '#000',
+              WebkitTextStrokeWidth: '0.2px',
+              WebkitTextStrokeColor: '#000',
+              fontSize: '16px',
+              margin: '0',
+              marginTop: '30px',
+            }}
+          >
+          나의 가게 정보
+          </p>
+         <StoreInfoContainer />
+         <RegisterButton>등록</RegisterButton>
+      </MainContainer>
+    </Container>
+  )
 }
